88f3e8c6514dd7b8ce3d4de505f91cedadb5f422
alymamdouh <Aly.Mamdouh@mdi.com.eg>
2026-06-16 00:07:30 +0300

    Refactor connection strings and update launch settings for local development

--- examples/webApi/LearnDocker.Api/Program.cs
+++ examples/webApi/LearnDocker.Api/Program.cs
@@ -9,13 +9,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("NeonConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
